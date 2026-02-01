6f0d5fe86bc81929db693cea8a1e26d2d530fbad
DrainBread <joaocesario@yandex.com>
2026-02-01 17:05:20 -0300

    added basic (and likely clunky) ore generation, minus coral polyp

---
+++ src/main/java/net/blueasclepias/bejeweled/datagen/ModWorldGenProvider.java
@@ -0,0 +1,4 @@
+package net.blueasclepias.bejeweled.datagen;
+
+public class ModWorldGenProvider {
+}
---
+++ src/main/java/net/blueasclepias/bejeweled/record/OreGenSettings.java
@@ -0,0 +1,8 @@
+package net.blueasclepias.bejeweled.oredata;
+
+public record OreGenSettings (
+    int veinSize,
+    int veinsPerChunk,
+    int minY,
+    int maxY
+){}
